1784a820566cc6ec6c6c9068e64e54ee1f0ddf8f
SurjeshYadavMasai <surjeshom@gmail.com>
2022-05-21 23:11:01 +0530

    GitHub Repo

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import RecreatedGithub from './components/RecreatedGithhub';
+
+function App() {
+  return (
+    <div className="App">
+      <RecreatedGithub/>
+    </div>
+  );
+}
+
+export default App;
